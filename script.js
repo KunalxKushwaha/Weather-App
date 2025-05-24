ae5d3dbd1bd68aef252028d27b7f8a08575429d2
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-05-25 00:35:41 +0530

    Backend is been added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const cityInput = document.querySelector('.inp-city');
 const searchButton = document.querySelector('.srch-button');
+const apiKey = `f75a076ddfd3036a93271f06bccada56`
+const notFound = document.querySelector('.not-found');
 
 searchButton.addEventListener('click', () => {
     if(cityInput.value.trim() === '') {
@@ -8,8 +10,33 @@ searchButton.addEventListener('click', () => {
     else{
        
         cityInput.blur();
-        console.log(cityInput.value);
+        updateWeather(cityInput.value);
          cityInput.value = ''
-    }
-    
+    }   
 });
+
+cityInput.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter') {
+        if(cityInput.value.trim() === '') {
+            alert('Please enter a city name');
+        } else {
+            cityInput.blur();
+            updateWeather(cityInput.value);
+            cityInput.value = '';
+        }
+    }
+});
+
+ async function getFetchData(endPoint, city) {
+    const apiKeyURL= `https://api.openweathermap.org/data/2.5/forecast?q=Meerut&appid=${apiKey}&units=metric`;
+    const response = await fetch(apiKeyURL)
+        return response.json()
+}
+async function updateWeather(Weather,city) {
+    const weatherData = await getFetchData(Weather,city);
+
+    if(weatherData.cod !== '200') {
+        showDislplaySection(notFound)
+    }
+    console.log(weatherData);
+}
